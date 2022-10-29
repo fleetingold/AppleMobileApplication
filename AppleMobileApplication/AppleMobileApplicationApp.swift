7ad92329ad39476e99444c9c0e482aa949aa401d
fleetingold <1161851523@qq.com>
2022-10-29 15:51:25 +0800

    Initial Commit

---
+++ AppleMobileApplication/AppleMobileApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMobileApplicationApp.swift
+//  AppleMobileApplication
+//
+//  Created by fleetingold on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct AppleMobileApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
